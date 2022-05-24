7b984ce3abd2324ae8102d42f46f9d741605a834
dayumm111 <100253910+Destroyer701@users.noreply.github.com>
2022-05-24 15:07:26 +0530

    A simple discord bot

---
+++ example.py
@@ -0,0 +1,24 @@
+import discord
+import discord.ext.commands
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = your_bot_prefix, help_command=None)
+
+token = ''
+# Your bot token
+# DONT LEAK THE TOKEN TO ANYONE ! THEY CAN GET FULL ACCESS OVER YOUR BOT
+
+your_bot_prefix = ""
+# Your bot prefix
+
+# A bot command
+@bot.command()
+async def newcommand(ctx):
+    await ctx.send("Hello! I am a bot")
+    
+    
+bot.run(token)
+# VERY IMPORTANT
+# Runs the bot 
+# DONT LEAK THE TOKEN TO ANYONE ! THEY CAN GET FULL ACCESS OVER YOUR BOT
+    
