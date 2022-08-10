0b057adaad41d8a973f0c2a03f32658a41abcc18
Shrek <100253910+Shrek069@users.noreply.github.com>
2022-08-10 18:52:13 +0530

    Fixed Variables
    Added the prefix variable to the top.

--- example.py
+++ example.py
@@ -2,14 +2,16 @@ import discord
 import discord.ext.commands
 from discord.ext import commands
 
+your_bot_prefix = ""
+# Your bot prefix
+
 bot = commands.Bot(command_prefix = your_bot_prefix, help_command=None)
 
 token = ''
 # Your bot token
 # DONT LEAK THE TOKEN TO ANYONE ! THEY CAN GET FULL ACCESS OVER YOUR BOT
 
-your_bot_prefix = ""
-# Your bot prefix
+
 
 # A bot command
 @bot.command()
